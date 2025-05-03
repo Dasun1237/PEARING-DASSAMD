7fbd7a3349efeaa1560cba39697716a78c03c31f
Dasun1237 <pushpartx7@gmail.com>
2025-05-03 09:02:41 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,12 @@
+module.exports = {
+    PREFIX: "DASSA-MD [KILL]>>>",
+    SUCCESS_NUMBERS: ["94743277981"], 
+    SUCCESS_MESSAGE: "*HELLO DASSA MD V2 OWNER 🚀*",
+    CONNECTED_MESSAGE: `
+┏━━━━━━━━━━━━━━
+┃📍 *DASSA MD SESSION ID*
+┃📍 *SUCCESSFULLY*
+┃📍 *CONNECTED ✅🔥*
+┗━━━━━━━━━━━━━━━
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬`
+};
